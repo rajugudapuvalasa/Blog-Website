34cdaedcb6b7ef422851cc4590a9242db9bb4af9
rajugudapuvalasa <rajugudapuvalasa@gmail.com>
2025-08-22 17:13:54 +0530

    update recent

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,18 +1,16 @@
+// multer.js
 const multer = require('multer');
-const path = require('path');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary');
 
-const storage = multer.diskStorage({
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); 
-  }
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'blog_images', // folder in Cloudinary
+    allowed_formats: ['jpeg', 'jpg', 'png'],
+  },
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = /jpeg|jpg|png/;
-  const ext = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-  const mime = allowedTypes.test(file.mimetype);
-  if (ext && mime) cb(null, true);
-  else cb(new Error('Only JPEG, JPG, and PNG files are allowed'), false);
-};
+const upload = multer({ storage });
 
-module.exports = multer({ storage, fileFilter });
+module.exports = upload;
